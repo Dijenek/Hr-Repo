cba2b145b854436935d0c564b659146548e1a95e
Marko Stankov <m.stankov@vegaitsourcing.rs>
2017-10-31 15:11:46 +0100

    Added text field

---
+++ ReadyTest/Models/Fields/TextField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReadyTest.Models.Fields
+{
+    public class TextField
+    {
+        public int Id { get; set; }
+        public Employee employee { get; set; }
+        public string Name { get; set; }
+        public FieldTypes fieldType { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Maximum length must be numeric")]
+        public int maxLength { get; set; }
+        public string defaultValue { get; set; }
+    }
+}
